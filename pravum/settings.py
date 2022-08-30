f193755a6dabc39bfcafe7036d21e656edb8f89e
Taras <emporio86@gmail.com>
2022-08-31 01:42:39 +0300

    updated ALLOWED_HOST to all

--- pravum/settings.py
+++ pravum/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 import sys
-import dj_database_url
+# import dj_database_url
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
