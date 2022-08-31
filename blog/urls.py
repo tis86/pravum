b56982f1f2f0f688ede4d7b07b76c5f523ac4946
Taras <emporio86@gmail.com>
2022-08-31 18:52:12 +0300

    updated urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
         path('', views.PostList.as_view(), name='home'),
         path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+        path('admin/', admin.site.urls),
+        path('', include('blog.urls')),
 ]
 
 
